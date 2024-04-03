2f6af45cd4ced23484d1c4e308f8f229035cba7c
Lezvie <steklovrazor@yandex.ru>
2024-04-03 17:28:55 +0500

    Spring_Task_5

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,33 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarServiceImpl implements CarService {
+    private final List<Car> listCars = new ArrayList<>();
+
+    {
+        listCars.add(new Car("mers", 2015, "blue"));
+        listCars.add(new Car("bmw", 2012, "green"));
+        listCars.add(new Car("opel", 2003, "white"));
+        listCars.add(new Car("vaz", 2023, "black"));
+        listCars.add(new Car("audi", 2011, "purple"));
+    }
+
+    @Override
+    public List<Car> getListByCars(int count) {
+        return listCars.stream()
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Car> getAllCars() {
+        return listCars;
+    }
+}
